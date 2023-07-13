02cc800eed64a537fa9dd6299499e297a959f1aa
Pereira <126605420+WPereiraJ@users.noreply.github.com>
2023-07-12 21:38:20 -0300

    Update alterar_foto.php

--- PHP/alterar_foto.php
+++ PHP/alterar_foto.php
@@ -23,10 +23,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                 // Mover o arquivo para o diretório de destino
                 if (move_uploaded_file($foto["tmp_name"], $diretorio_destino . $nome_arquivo)) {
-                        $servername = "sql10.freemysqlhosting.net";
-                        $username = "sql10624365";
-                        $password = "XMdT1yBz3V";
-                        $dbname = "sql10624365";
+                        $servername = "";
+                        $username = "";
+                        $password = "";
+                        $dbname = "";
 
                     $conn = new mysqli($servername, $username, $password, $dbname);
 
